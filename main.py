526dd2c950d68b0abb7a89dae4912f6c40abe278
ruslan <ruslanzakharov3008@gmail.com>
2022-09-13 15:21:23 +0300

    Добавлена обработка POST /imports

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import flask_sqlalchemy
 from flask import Flask, jsonify, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, abort
 from flask_sqlalchemy import SQLAlchemy
+
+from datetime import datetime as dt
 
 app = Flask(__name__)
 api = Api(app)
@@ -28,7 +31,7 @@ class Parent(db.Model):
     )
     parent_id = db.Column(
         db.String,
-        db.ForeignKey('Items.id', ondelete='CASCADE')
+        db.ForeignKey('Items.id', ondelete='CASCADE'),
     )
 
 
@@ -43,5 +46,37 @@ class History(db.Model):
     date = db.Column(db.DateTime, primary_key=True)
 
 
+class Imports(Resource):
+    def post(self):
+        req = request.json
+
+        try:
+            for item in req['items']:
+                new_item = Item(
+                    id=item['id'],
+                    type=item['type'],
+                    size=item['size']
+                )
+                parent = Parent(
+                    item_id=item['id'],
+                    parent_id=item['parentId']
+                )
+                history = History(
+                    item_id=item['id'],
+                    date=dt.strptime(
+                        req['updateDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
+                )
+                for obj in [new_item, parent, history]:
+                    db.session.add(obj)
+                db.session.commit()
+        except:
+            db.session.rollback()
+            return {'code': 400, 'message': 'Validation Failed'}, 400
+
+        return '', 200
+
+
+api.add_resource(Imports, '/imports')
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
